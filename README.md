0415b6406ae08a0d350a8dc63e835d61998c703c
David Morales <dmorales@odec.es>
2022-02-04 00:05:06 +0100

    Corrections and level 1

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
+## ESTRUCTURA 
+<br>
+Per poder entendre l'estructura de les BBDD als fitxers .png s'ha de tenir en compte que els objectes amb la part superior de color verd són col·leccions. I els de color blau són embedded documents (o arrays) dintre del document base de la col·lecció.
+
+Altres notes important són que a les relacions entre document i embedded document hi ha un petit text que indica si és un altre document (obj) o un array (ary).   
+Els elements embedded, a més, tenen una fulla a la part superior dreta del seu requadre. Si la fulla és verda, es tracta d'un document directament dintre del document original. Però si la fulla és marró significarà que es refereix a un array amb objectes del tipus definit a eixe requadre.   
+Per últim, alguns camps tenen definides unes fletxes a la banda dreta. Aquestes fletxes indiquen una relació, habitualment com a clau forània entre dues col·leccions.
+
+Per a l'exercici de l'òptica no hi ha massa a comentar, ja que és bastant simple i s'ha pogut agrupar tot amb una única col·lecció i embedded documents.   
+Però per al cas de la pizzeria, s'ha buscat una aproximació en la qual per als elements més recurrents es creen embedded documents reduïts. Aquests tenen la informació més clau i el ID per referenciar el document a la col·lecció pertinent on es trobarà tota la informació. D'aquesta forma, amb una única consulta es pot recuperar el "overview" de la situació (per exemple una comanda a la pizzeria) i tindrem el nom del client, de l'empleat... Però ja pensant en la web, si es volgués a partir de la comanda fer click al nom de l'empleat que l'ha servida i veure més informació d'ell, es tindria el ID per fer una query i extraure el detall concret d'aquest empleat.
+
+
+<br>   
+<hr>   
+
+## EXECUCIÓ
+<br>
 Per a executar un dels fitxers simplement s'haurà de fer ús del comandament mongo (s'ha de tenir mongo en marxa al port per defecte) seguit del nom de la BBDD i després el nom del fitxer. Per al cas de l'òptica seria:
 
 ```bash
--- pizzeria.js
+++ pizzeria.js
@@ -2,102 +2,101 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
+function generateUUID() { // Public Domain/MIT
+    var d = new Date().getTime();//Timestamp
+    var d2 = ((typeof performance !== 'undefined') && performance.now && (performance.now()*1000)) || 0;//Time in microseconds since page-load or 0 if unsupported
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16;//random number between 0 and 16
+        if(d > 0){//Use timestamp until depleted
+            r = (d + r)%16 | 0;
+            d = Math.floor(d/16);
+        } else {//Use microseconds since page-load if supported
+            r = (d2 + r)%16 | 0;
+            d2 = Math.floor(d2/16);
+        }
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+}
+
+  
 // Removing database to not duplicate documents
 db.dropDatabase();
 
 
-// Province
-for (let i = 1; i <= 5; i++) {
-    db.province.insert({_id: i, name: `Province ${i}`});
-}
 
-// City 
-for (let i = 1; i <= 15; i++) {
-    let province = getRandomInt(5) + 1;
-    db.city.insert({_id: i, name: `City ${i}`, province: `Province ${province}`, id_province: province});
-}
 
-// Product
-const products = ['Pizza', 'Hamburger', 'Drink'];
+// Order
+const productOptions = ['Pizza', 'Hamburger', 'Drink'];
 const categories = ['Italian', 'American', 'Bullshit']
+const types = ['Take away', 'Delivery'];
 for (let i = 1; i <= 15; i++) {
-    let product = products[getRandomInt(products.length-1)];
-    db.product.insert({
-        _id: i, 
-        name: `${product} ${i}`, 
-        type: product,
-        description: `Description for ${product}`,
-        image: 'https://picsum.photos/200/300',
-        price: getRandomInt(100),
-        category: product === 'Pizza' ? categories[getRandomInt(categories.length-1)] : '',
+    
+    let type = types[getRandomInt(types.length)];    
+    let products = [];
+
+    for (let z = 1; z <= getRandomInt(5); z++) {
+        let product = productOptions[getRandomInt(productOptions.length)];    
+
+        products.push({
+            type: product, 
+            name: `${product} ${z}`,
+            description: "No sense description",
+            image_url: 'https://picsum.photos/200/300',
+            prize: getRandomInt(20),
+            category: product === 'Pizza' ? `Category ${getRandomInt(3)}` : null,
+        });
+    }
+
+    db.order.insert({
+        ordered_at: new Date(),
+        type,
+        prize: getRandomInt(100),
+        delivery_date: type === 'Delivery' ? new Date() : null,
+        client: {
+            name: `Client ${i}`,
+            id_client: generateUUID()
+        },
+        employee: {
+            name: `Employee ${i}`,
+            id_employee: generateUUID()
+        },
+        products
+    });
+}
+
+// Client
+for (let i = 1; i <= 10; i++) {
+    db.client.insert({
+        name: `Client ${i}`,
+        surnames: `Surnames ${i}`,
+        address: `Address ${i}`,
+        postal_code: `Postal code ${i}`,
+        province: `Province ${getRandomInt(20)}`,
+        city: `City ${getRandomInt(50)}`,
     });
 }
 
 // Shop
 for (let i = 1; i <= 10; i++) {
-    let province = getRandomInt(5) + 1;
-    let city = getRandomInt(15) + 1;
+    
+    let employees = [];
+    for (let z = 1; z <= getRandomInt(5); z++) {
+        employees.push({
+            name: `Employee ${i}`,
+            surnames: `Surname ${i}`,
+            nif: `0000000Z`,
+            phone: '999999999',
+            job: i % 2 === 0 ? 'Kitchen' : 'Delivery'
+        });
+    }
+
     db.shop.insert({
-        _id: i, 
-        address: `Invented Street ${i}`,
-        province: `Province ${province}`,
-        city: `City ${city}`,
-        id_province: province,
-        id_city: city,
+        address: `Address ${i}`,
+        postal_code: `Postal code ${i}`,
+        province: `Province ${getRandomInt(20)}`,
+        city: `City ${getRandomInt(50)}`,
+        employees
+
     });
 }
 
-// Employee
-for (let i = 1; i <= 10; i++) {
-    let shop = getRandomInt(10) + 1;
-    db.employee.insert({
-        _id: i, 
-        name: `Employee ${i}`,
-        surname: `Surname ${i}`,
-        id_shop: shop,
-        nif: `0000000Z`,
-        phone: '999999999',
-        job: i % 2 === 0 ? 'Kitchen' : 'Delivery'
-    });
-}
-
-// Order 
-for (let i = 1; i <= 20; i++) {
-    let employee = getRandomInt(10) + 1;
-    db.order.insert({
-        _id: i,
-        event_date: new Date(),
-        type: i % 2 === 0 ? 'Takeaway' : 'Delivery',
-        price: getRandomInt(100),
-        id_shop: getRandomInt(10) + 1,
-        delivered_at: i % 2 === 0 ? null : new Date(),
-        id_employee: i % 2 === 0 ? null : employee,
-        employee: i % 2 === 0 ? null : `Employee ${employee}`,
-        products: Array.from({length: getRandomInt(13) + 1}, () => getRandomInt(15) + 1)
-    });
-}
-
-// Client
-for (let i = 1; i <= 15; i++) {
-    let city = getRandomInt(15) + 1;
-    let province = getRandomInt(5) + 1;
-    db.client.insert({
-        _id: i,
-        name: `Client ${i}`,
-        surname: `Surname ${i}`,
-        address: `Invented Street ${i}`,
-        postal_code: `00000`,
-        city: `City ${city}`,
-        id_city: city,
-        province: `Province ${province}`,
-        id_province: province,
-        phone: '999999999',
-        orders: Array.from({length: getRandomInt(3) + 1}, () => getRandomInt(20) + 1).map(idOrder => {
-            return {
-                id_order: idOrder,
-                event_date: new Date(),
-                price: getRandomInt(100)
-            }
-        })
-    });
-}
